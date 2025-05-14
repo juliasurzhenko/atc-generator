9de5b26663a00d509d64a9814579bf55a4d02aff
juliasurzhenko <surzhenkoyulia@gmail.com>
2025-05-14 09:27:13 +0300

    upd cors options

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: process.env.ORIGIN, //'http://localhost:5173' ,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type,Authorization', 
